c55284bf62b5073162f851d9ac1b82dd72507836
CROIRE-Aline <niyocroirealine@gmail.com>
2024-05-13 09:20:31 +0200

    changing the connection string

--- backend/connection/database.js
+++ backend/connection/database.js
@@ -3,7 +3,7 @@ const dotenv = require ("dotenv");
 dotenv.config();
 
 const Configs = {
-    MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017/Procurement',
+    MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING || 'mongodb+srv://niyocroirealine:I3lWKD8AmW27MmyG@cluster0.ufciukm.mongodb.net/Procurement',
     PORT: process.env.PORT ||8080,
     JWT_SECRET: process.env.JWT_SECRET || "secret" ,
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
