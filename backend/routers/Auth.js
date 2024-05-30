781e22ed8dcf7b7585681e45013517ca4d147555
vanessa <vanessabewe@gmail.com>
2024-05-30 09:58:40 +0200

    added resrtiction

--- backend/routers/Auth.js
+++ backend/routers/Auth.js
@@ -35,37 +35,37 @@ router.post("/reset-password", resetPassword);
 // Apply authMiddleware to routes that require authentication
 // ###############userroutes##############
 
-router.post("/user",  UserController.createUser);
-router.put("/user",  UserController.updateUser);
-router.delete("/user",  UserController.deleteUser);
-router.get("/user",  UserController.getAllUser);
+router.post("/user",authMiddleware,isAdmin,  UserController.createUser);
+router.put("/user", authMiddleware,isAdmin, UserController.updateUser);
+router.delete("/user",authMiddleware,isAdmin,  UserController.deleteUser);
+router.get("/user",authMiddleware,isAdmin,  UserController.getAllUser);
 
 
 
 
 // requistion routers
-router.post('/create', requestController.createRequest);
-router.get('/allRequest', requestController.getAllRequests);
-router.get('/request/:id', requestController.getRequestById);
-router.post('/:id/status', requestController.updateRequestStatus);
-router.post('/update-request', requestController.updateRequest);
-router.delete('/delete-request', requestController.deleteRequest);
+router.post('/create',authMiddleware,isHOD, requestController.createRequest);
+router.get('/allRequest', authMiddleware,isHOD, requestController.getAllRequests);
+router.get('/request/:id',authMiddleware,isHOD,  requestController.getRequestById);
+router.post('/:id/status', authMiddleware,isHOD, requestController.updateRequestStatus);
+router.post('/update-request',authMiddleware,isHOD,  requestController.updateRequest);
+router.delete('/delete-request', authMiddleware,isHOD, requestController.deleteRequest);
 
 
 
 //application routes
 router.post('/form',upload.single('cv'), submitForm);
 //router.post('/form', upload.single('cv'), createApplicationForm);
-router.post('/select-application',selectApplication)
-router.post('/reject-application',deleteRejectedApplications)
-router.get('/candidate',getApplicants)
+router.post('/select-application',authMiddleware,isHOD,selectApplication)
+router.post('/reject-application',authMiddleware,isHOD,deleteRejectedApplications)
+router.get('/candidate',authMiddleware,isHOD,getApplicants)
 
 //tenders
 
-router.post('/tender', uploads.single('image'), newTender)
-router.post('/select-tender',selectTender)
-router.post('/reject-tender',rejectTender)
-router.put('/update-tender',updateTender)
+router.post('/tender',authMiddleware,isAdmin,   uploads.single('image'), newTender)
+router.post('/select-tender',authMiddleware,isAdmin,  selectTender)
+router.post('/reject-tender',authMiddleware,isAdmin,  rejectTender)
+router.put('/update-tender',authMiddleware,isAdmin,  updateTender)
 router.get('/tenders',tenders)
 
 
